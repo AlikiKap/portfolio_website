1ce697e749cd47ba696e0738eaca1f8c82a0c829
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2023-08-16 15:07:09 +0300

    Added basename to BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
       <Route path="/" element={<App/>}>
           <Route index element={<Home/>}/>
