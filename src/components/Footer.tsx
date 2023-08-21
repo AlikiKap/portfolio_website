97447e3109138e721c14bd3f73904efdd9c9973b
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2023-08-21 11:37:54 +0300

    Buttons style was changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import { Button, Stack } from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 
 import React from 'react'
@@ -12,8 +13,9 @@ function Footer() {
             justifyContent="center"
             alignItems="center"
             flexDirection="row">
-        <Button variant='contained' startIcon={<LinkedInIcon/>} style={{background: '#02659a', borderRadius: '32px'}} href='https://www.linkedin.com/in/aliki-kapasakalidi-921891203'>LINKEDIN</Button>
-        <Button variant='contained' startIcon={<GitHubIcon/>} style={{background: '#8b31aa', borderRadius: '32px'}} href='https://github.com/AlikiKap '>GITHUB</Button>
+        <Button variant='outlined' startIcon={<LinkedInIcon/>} style={{border:'2px solid #02659a', borderRadius: '32px', margin:'8px', color: 'white'}} href='https://www.linkedin.com/in/aliki-kapasakalidi-921891203'>LINKEDIN</Button>
+        <Button variant='outlined' startIcon={<GitHubIcon/>} style={{border:'2px solid #8b31aa', borderRadius: '32px', margin:'8px', color: 'white'}} href='https://github.com/AlikiKap '>GITHUB</Button>
+        <Button variant='outlined' startIcon={<EmailIcon/>} style={{border:'2px solid #de4739',borderRadius: '32px', margin:'8px', color: 'white'}} href='mailto:aliki02.kapasakalidi@gmail.com'>E-MAIL</Button>
     </Stack>
     </>
   )
