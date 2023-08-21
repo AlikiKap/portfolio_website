f093afb7627115e3e22037da088e649beca7a063
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2023-08-21 11:37:02 +0300

    Email link and Download Resume button were added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import {Grid, Stack, Typography} from '@mui/material'
-import pfp from './icon.png'
+import {Button, Grid,Link, Stack, Typography} from '@mui/material'
+import avatar from './icon.png';
+
 
 function Banner() {
   const linearGradientStyle = {
@@ -16,7 +17,7 @@ function Banner() {
         direction="row"
         color="#D45BFF"
       >
-        <div>
+        <div style={{width:'500px'}}>
         <Typography 
         fontSize="24px"
         fontWeight="600"
@@ -27,37 +28,18 @@ function Banner() {
           >
             front-end developer 
           </mark> 
-          searching for a place where I can:
+          searching for a place where I can expand my knowledge and skills while working on interesting and important projects with a team.
 </Typography>
-<ul>
-  <li>
-  <Typography         
+<Typography         
         fontSize="24px"
         fontWeight="600"
         >
-        Work in a team
-    </Typography>
-  </li>
-  <li>
-  <Typography         
-        fontSize="24px"
-        fontWeight="600"
-        >
-      Learn new things everyday
-    </Typography>
-  </li>
-  <li>
-    <Typography         
-        fontSize="24px"
-        fontWeight="600"
-        >
-      Take part in working on interesting projects that solve real life problems
-    </Typography>
-  </li>
-</ul>
-
+        Contact me: 
+</Typography>
+<Link underline="always" style={{ color: '#BEFF34',fontSize:"24px",fontWeight:"400" }} href='mailto:aliki02.kapasakalidi@gmail.com'>aliki02.kapasakalidi@gmail.com</Link>
+<Button variant='outlined' style={{border:'2px solid #BEFF34',color:'white',fontSize:"18px",fontWeight:"400", margin:'12px'}} href='./CV_Aliki_Kapasakalidi.pdf' download>Download Resume</Button>
         </div>
-      <img src={pfp} alt='aaa' style={{ width: '700px', height: 'auto' }} />
+      <img src={avatar} alt='avatar-picture' style={{ width: '400px', height: 'auto' }} />
 
       </Stack>
   </div>
